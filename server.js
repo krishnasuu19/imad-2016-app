61c4e55ba617faedfec08dda82e9e89df95021e5
krishnasuu19 <su.music.vm@gmail.com>
2016-10-16 14:45:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,64 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+
+var articleOne = {
+    title: "Article One",
+    date: "oct 15",
+    heading: "Article one: ",
+    content: `
+             <p>
+                this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!
+            </p>
+            <p>
+                this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!
+            </p>
+             <p>
+                this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!
+            </p>
+             <p>
+                this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!this si the ocntent that will apear in my article 1!
+            </p>`
+    
+};
+function createTemp (data) {
+            var date = data.date;
+            var heading = data.heading;
+            var content = data.content;
+            var title = data.title;
+            var templateHtml= `
+                <html>
+                <head>
+                    <title>
+                       ${title}
+                    </title>
+                    <meta name="viewport" content="width=device-width, intial-scale=1" />
+                    <link href="/ui/style.css" rel="stylesheet" />
+                    
+                </head>
+                <body>
+                    <div class="container">
+                        
+                    
+                        <div>
+                            <a href='/'>Home</a>
+                        </div>
+                        <hr/>
+                    <h3>
+                        ${heading};
+                    </h3>
+                    <div>
+                      ${date};
+                    </div>
+                    <div>
+                      ${content};
+                    </div>
+                </div>
+                </body>
+            </html>`;
+        return templateHtml;
+}
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -10,7 +68,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+ res.send(createTemp(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
