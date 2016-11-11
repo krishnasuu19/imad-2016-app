b1fe7e8a1f3d83cbd9cd1721bb6674063384022f
krishnasuu19 <su.music.vm@gmail.com>
2016-11-11 23:16:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = "Changed";
 ///to make move
 
 var madi = document.getElementById('madi');
-img.onclick = function() {
-    img.style.marginleft = '100px';
+madi.onclick = function() {
+    madi.style.marginleft = '100px';
 };
 
